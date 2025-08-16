d695c991d9b6b6f69f0cfd61347a03308f95f14c
Anton Ryabenko <djedra@yandex.ru>
2025-08-16 18:44:36 +0700

    Updates

--- main.js
+++ main.js
@@ -1,88 +1,59 @@
 /******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
-
-/***/ 768:
-/***/ (() => {
-
-
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
+/******/ 	"use strict";
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	/* webpack/runtime/publicPath */
 /******/ 	(() => {
-/******/ 		// getDefaultExport function for compatibility with non-harmony modules
-/******/ 		__webpack_require__.n = (module) => {
-/******/ 			var getter = module && module.__esModule ?
-/******/ 				() => (module['default']) :
-/******/ 				() => (module);
-/******/ 			__webpack_require__.d(getter, { a: getter });
-/******/ 			return getter;
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 		__webpack_require__.p = "";
 /******/ 	})();
 /******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be in strict mode.
-(() => {
-"use strict";
 
-// EXTERNAL MODULE: ./src/js/GoblinGame.js
-var GoblinGame = __webpack_require__(768);
-var GoblinGame_default = /*#__PURE__*/__webpack_require__.n(GoblinGame);
+;// CONCATENATED MODULE: ./src/img/goblin.png
+const goblin_namespaceObject = __webpack_require__.p + "5089de856a7e39c47fa7.png";
+;// CONCATENATED MODULE: ./src/js/GoblinGame.js
+
+class GoblinGame {
+  constructor() {
+    this.gridSize = 4;
+    this.cells = [];
+    this.goblin = document.createElement("img");
+    this.goblin.src = goblin_namespaceObject;
+    this.goblin.classList.add("goblin");
+    this.currentPosition = -1;
+  }
+  init() {
+    const grid = document.querySelector(".game-grid");
+    for (let i = 0; i < this.gridSize * this.gridSize; i++) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      grid.appendChild(cell);
+      this.cells.push(cell);
+    }
+    this.moveGoblin();
+    setInterval(() => this.moveGoblin(), 1000);
+  }
+  moveGoblin() {
+    let newPosition;
+    do {
+      newPosition = Math.floor(Math.random() * this.cells.length);
+    } while (newPosition === this.currentPosition);
+    this.cells[newPosition].appendChild(this.goblin);
+    this.currentPosition = newPosition;
+  }
+}
+/* harmony default export */ const js_GoblinGame = (GoblinGame);
 ;// CONCATENATED MODULE: ./src/js/app.js
 
 document.addEventListener("DOMContentLoaded", () => {
-  const game = new (GoblinGame_default())();
+  const game = new js_GoblinGame();
   game.init();
 });
 ;// CONCATENATED MODULE: ./src/index.js
 
-
-})();
 
 /******/ })()
 ;
